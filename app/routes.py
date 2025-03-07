da63acbfa2cc431f0565bab84aafa66620301f18
Pavel Soroka <40a.pvl@gmail.com>
2025-03-07 12:58:46 +0200

    Adcd templates/

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,26 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'paalso'}
+    return render_template(
+        'index.html',
+        user=user
+    )
+
+@app.route('/posts')
+def posts_index():
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('posts_index.html', posts=posts)
