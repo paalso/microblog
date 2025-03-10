86d17b7df01ec4df47a773ee4eef5b621f82eb65
Pavel Soroka <40a.pvl@gmail.com>
2025-03-10 13:07:29 +0200

    WIP

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,12 @@
 from flask import (
+    flash,
     jsonify,
+    redirect,
     render_template
 )
+
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -27,6 +31,16 @@ def posts_index():
         }
     ]
     return render_template('posts_index.html', posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
 
 
 # TODO: remove after debugging
