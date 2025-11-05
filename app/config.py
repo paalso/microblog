c3617c2e1d3545909177a6437ec7a882ab38ad9e
Pavel Soroka <40a.pvl@gmail.com>
2025-11-05 19:07:01 +0200

    Implement db settings

--- app/config.py
+++ app/config.py
@@ -1,5 +1,13 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+# e.g.
+# __file__: /home/paalso/Courses/microblog/app/config.py
+# os.path.dirname(__file__): /home/paalso/Courses/microblog/app
+# basedir: /home/paalso/Courses/microblog/app
+
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = (os.environ.get('DATABASE_URL') or
+                               'sqlite:///' + os.path.join(basedir, 'app.db'))
