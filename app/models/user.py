69bc509cc8dba2f868c98bfd6d28be283e7eafc0
Pavel Soroka <40a.pvl@gmail.com>
2025-11-06 13:51:35 +0200

    UPD app.models.user.User

--- app/models/user.py
+++ app/models/user.py
@@ -2,12 +2,14 @@ from typing import Optional
 
 import sqlalchemy as sa
 import sqlalchemy.orm as so
+from flask_login import UserMixin
+from werkzeug.security import check_password_hash, generate_password_hash
 
 from app import db
 from app.models.mixins import TimestampMixin
 
 
-class User(TimestampMixin, db.Model):
+class User(TimestampMixin, UserMixin, db.Model):
     __tablename__ = 'users'
 
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -19,6 +21,12 @@ class User(TimestampMixin, db.Model):
 
     posts: so.WriteOnlyMapped['Post'] = so.relationship(        # noqa: F821
         'Post', back_populates='author')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
