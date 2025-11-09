1adfc4937a33493af2e65bb9a892c940df297730
Pavel Soroka <40a.pvl@gmail.com>
2025-11-09 11:32:04 +0200

    Implemente user profile view

--- app/routes.py
+++ app/routes.py
@@ -84,6 +84,17 @@ def users():
     return render_template('admin/users.html', title='Users', users=users)
 
 
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = db.first_or_404(sa.select(User).where(User.username == username))
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
+
 @app.route('/debug')
 def debug():
     print("--- Request context info ---")
