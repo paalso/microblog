977d05e248acf6db4ac446e40cb178c548988cc7
Pavel Soroka <40a.pvl@gmail.com>
2025-11-11 16:59:59 +0200

    Add logging to file

--- app/__init__.py
+++ app/__init__.py
@@ -2,14 +2,16 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from app.logging_config import configure_logging
 
 from app.config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+configure_logging(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
-from app import routes
+from app import routes, errors
---
+++ app/logging_config.py
@@ -0,0 +1,26 @@
+import logging
+import os
+from logging.handlers import RotatingFileHandler
+
+# TODO: complete
+def configure_logging(app):
+    # if app.debug or app.testing:
+    #     return
+
+    log_dir = os.path.join(app.root_path, '..', 'logs')
+    os.makedirs(log_dir, exist_ok=True)
+
+    file_handler = RotatingFileHandler(
+        os.path.join(log_dir, 'microblog.log'),
+        maxBytes=10240,
+        backupCount=10,
+        encoding='utf-8'
+    )
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+    ))
+    file_handler.setLevel(logging.DEBUG)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
