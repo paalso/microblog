df8bcd557334a516ffea38bd5356bd2f2d5e877d
Pavel Soroka <40a.pvl@gmail.com>
2025-11-29 15:27:39 +0200

    UPD Config.POST_PER_PAGE setting

--- app/config.py
+++ app/config.py
@@ -5,9 +5,11 @@ basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_TRACK_MODIFICATIONS = (
+        os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS', False))
+    POSTS_PER_PAGE = os.environ.get('POSTS_PER_PAGE', 20)
+
     SQLALCHEMY_DATABASE_URI = (
         os.environ.get('DATABASE_URL')
         or 'sqlite:///' + os.path.join(basedir, 'instance', 'app.db')
     )
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    POSTS_PER_PAGE = 5
