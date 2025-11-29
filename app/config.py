44a7516b20d129833313774f3281ef1dcfdd4508
Pavel Soroka <40a.pvl@gmail.com>
2025-11-29 17:31:00 +0200

    Fix a type mismatch in Config.POST_PER_PAGE setting

--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_TRACK_MODIFICATIONS = (
         os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS', False))
-    POSTS_PER_PAGE = os.environ.get('POSTS_PER_PAGE', 20)
+    POSTS_PER_PAGE = int(os.environ.get('POSTS_PER_PAGE', 20))
 
     SQLALCHEMY_DATABASE_URI = (
         os.environ.get('DATABASE_URL')
