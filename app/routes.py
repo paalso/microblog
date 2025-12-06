871a8aa9a76f63ac7d0fa26704308e9743de020c
Pavel Soroka <40a.pvl@gmail.com>
2025-12-06 16:54:33 +0200

    WIP

--- app/routes.py
+++ app/routes.py
@@ -137,17 +137,17 @@ def reset_password_request():
 @main_bp.route('/reset_password/<token>', methods=['GET', 'POST'])
 def reset_password(token):
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('main.index'))
     user = User.verify_reset_password_token(token)
     if not user:
-        return redirect(url_for('index'))
+        return redirect(url_for('main.index'))
     form = ResetPasswordForm()
     if form.validate_on_submit():
         user.set_password(form.password.data)
         db.session.commit()
         flash('Your password has been reset.')
-        return redirect(url_for('login'))
-    return render_template('reset_password.html', form=form)
+        return redirect(url_for('main.login'))
+    return render_template('email/reset_password.html', form=form)
 
 
 @main_bp.route('/register', methods=['GET', 'POST'])
